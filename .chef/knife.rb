101b2b211e162ed45381dc70db501dbd59948851
David Laing <david@davidlaing.com>
2011-10-22 22:44:27 +0000

    Added settings for cucumber-chef

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -10,3 +10,9 @@ chef_server_url          "https://api.opscode.com/organizations/#{ENV['ORGNAME']
 cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
 cookbook_path            ["#{current_dir}/../cookbooks"]
+knife[:aws_access_key_id] = ENV['AWS_ACCESS_KEY_ID']
+knife[:aws_secret_access_key] = ENV['AWS_SECRET_ACCESS_KEY']
+knife[:identity_file] = ENV['AWS_SSH_KEY_PATH'] 
+knife[:aws_ssh_key_id] = ENV['AWS_SSH_KEY_ID']
+knife[:availability_zone] = "us-east-1a"
+knife[:region] = "us-east-1"
