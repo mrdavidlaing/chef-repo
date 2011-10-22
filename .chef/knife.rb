524aabe564bd1eb684290d1bb595dc8909e89a70
David Laing <david@davidlaing.com>
2011-10-22 22:31:53 +0000

    Fixed syntax error

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -3,7 +3,7 @@ user = ENV['OPSCODE_USER'] || ENV['USER']
 log_level                :info
 log_location             STDOUT
 node_name                user
-client_key               "#ENV['HOME']}/.chef/#{user}.pem"
+client_key               "#{ENV['HOME']}/.chef/#{user}.pem"
 validation_client_name   "#{ENV['ORGNAME']}-validator"
 validation_key           "#{ENV['HOME']}/.chef/#{ENV['ORGNAME']}-validator.pem"
 chef_server_url          "https://api.opscode.com/organizations/#{ENV['ORGNAME']}"
